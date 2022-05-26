16818f7716dc0927969ae64b6a051f80ccc3960b
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2022-05-25 22:42:05 -0300

    if and switch statements in go

--- src/app/app.go
+++ src/app/app.go
@@ -1,10 +1,32 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
+
+	exibeIntroducao()
+	exibeMenu()
+
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+
+	case 2:
+		fmt.Println("Exibindo logs...")
+
+	case 0:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheco este comando")
+	}
+}
+
+func exibeIntroducao() {
 	var nome string
-	var comando int
 	versao := 1.1
 
 	fmt.Printf("Qual seu nome? ")
@@ -12,14 +34,18 @@ func main() {
 
 	fmt.Println("Olá sr(a).", nome)
 	fmt.Println("Este programa está na versão", versao)
+}
 
+func exibeMenu() {
 	fmt.Println("Digite sua opção desejada:")
 	fmt.Println("1 - Iniciar monitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair do programa")
+}
 
-	//fmt.Scanf("%d", &comando)
-	// Scan infere a entrada baseado no tipo da variavel destino
-	fmt.Scan(&comando)
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
 
+	return comandoLido
 }
