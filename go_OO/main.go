4e2e636d61cafbf2a8c42b0739e4654b24d16d82
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2022-05-28 23:08:40 -0300

    create struct using pointers

--- go_OO/main.go
+++ go_OO/main.go
@@ -10,7 +10,21 @@ type ContaCorrente struct {
 }
 
 func main() {
+	// forma normal de usar struct
 	conta1 := ContaCorrente{"doglinha", 10, 123456, 4}
+	// forma passando o nome dos parametros
+	conta2 := ContaCorrente{
+		titular:       "doglao",
+		numeroAgencia: 20,
+		numeroConta:   1123654,
+		saldo:         40}
 
-	fmt.Println(conta1)
+	fmt.Println(conta1, conta2)
+
+	// forma coisando cada parametro
+	var conta3 *ContaCorrente   // cria um ponteiro para uma ContaCorrente
+	conta3 = new(ContaCorrente) // liga o ponteiro a uma ContaCorrente
+	conta3.titular = "doglas"
+
+	fmt.Println(conta3)
 }
