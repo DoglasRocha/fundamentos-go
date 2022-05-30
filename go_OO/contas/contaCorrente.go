78fb56ec14af95370fd5e5d358330b8fb0e0ed31
Doglas Rocha <79492860+DoglasRocha@users.noreply.github.com>
2022-05-30 00:08:44 -0300

    modulate the project

--- go_OO/contas/contaCorrente.go
+++ go_OO/contas/contaCorrente.go
@@ -1,17 +1,17 @@
 package contas
 
 type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
+	Titular       string
+	NumeroAgencia int
+	NumeroConta   int
+	Saldo         float64
 }
 
 func (c *ContaCorrente) Sacar(valorDoSaque float64) bool {
-	podeSacar := valorDoSaque <= c.saldo && valorDoSaque > 0
+	podeSacar := valorDoSaque <= c.Saldo && valorDoSaque > 0
 
 	if podeSacar {
-		c.saldo -= valorDoSaque
+		c.Saldo -= valorDoSaque
 		return true
 	}
 
@@ -21,11 +21,11 @@ func (c *ContaCorrente) Sacar(valorDoSaque float64) bool {
 func (c *ContaCorrente) Depositar(valorDoDeposito float64) (bool, float64) {
 
 	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return true, c.saldo
+		c.Saldo += valorDoDeposito
+		return true, c.Saldo
 	}
 
-	return false, c.saldo
+	return false, c.Saldo
 }
 
 func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *ContaCorrente) bool {
--- go_OO/main.go
+++ go_OO/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	c "banco/contas"
 	"fmt"
-	"go_OO/contas"
 )
 
 func main() {
-	conta1 := contas.ContaCorrente{"doglinha", 10, 123456, 1000}
-	conta2 := contas.ContaCorrente{"doglas", 20, 123654, 2000}
+	conta1 := c.ContaCorrente{"doglinha", 10, 123456, 1000}
+	conta2 := c.ContaCorrente{"doglas", 20, 123654, 2000}
 
 	fmt.Println(conta1, conta2)
-	fmt.Println(conta1.Transferir(2000, &conta2))
+	fmt.Println(conta2.Transferir(2000, &conta1))
 	fmt.Println(conta1, conta2)
 }
